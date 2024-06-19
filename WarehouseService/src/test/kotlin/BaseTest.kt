d6f3f9f163083e20203ba54538a72cf64159ac76
Elisa Albertini <elisa.albertini25@gmail.com>
2024-06-19 10:30:40 +0200

    chore: prova 10

--- WarehouseService/src/test/kotlin/BaseTest.kt
+++ WarehouseService/src/test/kotlin/BaseTest.kt
@@ -1,4 +1,5 @@
 import com.mongodb.client.model.Filters
+import com.mongodb.kotlin.client.coroutine.MongoClient
 import domain.Ingredient
 import io.kotest.core.spec.style.AnnotationSpec
 import server.MongoUtils
@@ -10,6 +11,13 @@ open class BaseTest : AnnotationSpec() {
     protected val coffee = Ingredient("coffee", 1)
     protected val notAvailableCoffee = Ingredient(coffee.name, 0)
     protected val ingredients = listOf(milk, tea)
+
+    @BeforeAll
+    suspend fun initializeCollection(){
+        MongoClient.create(MongoInfo().mongoAddress)
+            .getDatabase(MongoInfo().databaseName)
+            .createCollection("Ingredients")
+    }
 
     @BeforeEach
     suspend fun beforeTest() {
