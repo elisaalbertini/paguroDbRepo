434280a64013901ea3b865092fd438e51b228897
Elisa Albertini <elisa.albertini25@gmail.com>
2024-07-12 12:45:44 +0200

    chore: ciao

--- WarehouseService/src/main/kotlin/server/MainInit.kt
+++ WarehouseService/src/main/kotlin/server/MainInit.kt
@@ -12,15 +12,6 @@ import kotlinx.coroutines.runBlocking
 object MainInit {
     @JvmStatic
     fun main(args: Array<String>) {
-        val collection = MongoUtils.getMongoCollection(MongoInfo())
-        val milk = Ingredient("milk", 95)
-        val tea = Ingredient("tea", 0)
-        val ingredients = listOf(milk, tea)
-        runBlocking {
-            collection.deleteMany(Filters.empty())
-            collection.insertMany(ingredients)
-        }
-
         val vertxServer = Vertx.vertx()
         vertxServer.deployVerticle(Server(MongoInfo(), 8080))
     }
