918f8e87d32da5ea0338cb9382a7dc870ebc8621
Elisa Albertini <elisa.albertini25@gmail.com>
2024-07-15 17:52:26 +0200

    refactor: remove unused imports

--- menu-service/src/app.ts
+++ menu-service/src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import helmet from "helmet";
 import menuRouter from './routes/menu';
 
 const app = express()
-app.use(express.json())
+app.use(helmet())
+//app.use(express.json())
 
 //menu route
 app.use('/menu', menuRouter)
