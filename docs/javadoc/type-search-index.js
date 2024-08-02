29d4816a2d4405828e8900dcc24a13b49af8c8ac
elisaalbertini <66846693+elisaalbertini@users.noreply.github.com>
2024-08-02 16:05:44 +0000

    Deploying to report from @ elisaalbertini/paguroDbRepo@b9be23456a0b6eedd5d1cd0469c53a82af92cc6c 🚀

---
+++ docs/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Message"},{"p":"<Unnamed>","l":"OrderCard"},{"p":"<Unnamed>","l":"View"},{"p":"<Unnamed>","l":"WebSocketConnection"}]
