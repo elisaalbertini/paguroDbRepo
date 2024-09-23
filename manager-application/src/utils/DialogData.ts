7c07c2d2940f8b7df30eba86c66fb4b83c6f26d3
Elisa Albertini <elisa.albertini25@gmail.com>
2024-09-23 09:39:14 +0200

    chore: update 2

--- manager-application/src/utils/DialogData.ts
+++
@@ -1,9 +0,0 @@
-import { MatDialog } from "@angular/material/dialog";
-
-/**
- * This interface represents the data passed to a dialog
- */
-export interface DialogData {
-	ws: WebSocket,
-	dialog: MatDialog
-}
--- manager-application/src/utils/IArray.ts
+++
@@ -1,7 +0,0 @@
-/**
- * This interface represents an array that has 
- * a string as index and a number as a value
- */
-export interface IArray {
-	[index: string]: number;
-}
