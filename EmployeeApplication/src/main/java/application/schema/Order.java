7e14d0cbfe21a12b215678ecedfa4c5b2e9c946c
Elisa Albertini <elisa.albertini25@gmail.com>
2024-09-27 12:36:07 +0200

    chore: prova 25

--- EmployeeApplication/src/main/java/application/schema/Order.java
+++ EmployeeApplication/src/main/java/application/schema/Order.java
@@ -6,7 +6,7 @@ import java.util.List;
  * Order Schema
  *
  * @param _id of the order
- * @param customerEmail email of the customer that made the order qwertytrewqwertytrewqwertyrewq asdfghjhgfdsa
+ * @param customerEmail email of the customer that made the order qwertytrewqwertytrewqwertyrewq asdfghjhgfdsa drgzdgdrhzdrg
  * @param price total price of the order
  * @param type type of the order
  * @param state current state of the order
