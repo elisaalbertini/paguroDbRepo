3d985461fb9902f8f51e395a9910737a7537bd20
Elisa Albertini <elisa.albertini25@gmail.com>
2024-09-27 12:12:22 +0200

    chore: prova 23

--- server/src/utils/handlers.ts
+++ server/src/utils/handlers.ts
@@ -11,7 +11,7 @@ interface IArray {
 }
 
 /**
- * This function creates an error message given the code and the message returned by an API lalalalalalalal
+ * This function creates an error message given the code and the message returned by an API lalalalalalalala
  * @param code 
  * @param msg 
  * @returns a new response message with empty data
