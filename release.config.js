d611039ec81a2931d0220c197f3d4a7b751ff10b
Elisa Albertini <elisa.albertini25@gmail.com>
2024-11-28 15:20:21 +0100

    feat: tolgo message

--- release.config.js
+++ release.config.js
@@ -3,7 +3,6 @@ config.plugins.push(
     "@semantic-release/github",
      ["@semantic-release/git", {
       "assets": ["dist/**/*.{js,css}", "docs", "package.json"],
-      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }]
 )
 module.exports = config
