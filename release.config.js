56391114ac7807a16b43b023dfce2227a39702f2
Elisa Albertini <elisa.albertini25@gmail.com>
2024-11-28 11:03:30 +0100

    chore: prova1

--- release.config.js
+++ release.config.js
@@ -1,20 +1,6 @@
-var publishCmd = `
-./gradlew assemble -Pversion=\${nextRelease.version} --parallel || exit 1
-./gradlew createJavadoc -Pversion=\${nextRelease.version} || exit 2
-git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md || exit 3
-git push --force origin \${nextRelease.version} || exit 4
-echo ::set-output name=nextVer::\${nextRelease.version} || exit 5
-`
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
-    ["@semantic-release/exec", {
-        "publishCmd": publishCmd,
-    }],
-    ["@semantic-release/github", {
-        "assets": [ ]
-    }],
+    "@semantic-release/github",
     "@semantic-release/git",
 )
-config.branches = ['main']
-
 module.exports = config
