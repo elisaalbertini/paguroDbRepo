32f24fa08d074f15ba41e84329d33e7581545098
Elisa Albertini <66846693+elisaalbertini@users.noreply.github.com>
2024-11-28 12:42:50 +0100

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -1,6 +1,9 @@
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
     "@semantic-release/github",
-    "@semantic-release/git",
+     ["@semantic-release/git", {
+      "assets": ["dist/**/*.{js,css}", "docs", "package.json"],
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }]
 )
 module.exports = config
