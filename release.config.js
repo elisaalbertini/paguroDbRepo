940f7ba84a0cd4e5c5b954eab75f54ab7718a49e
Elisa Albertini <elisa.albertini25@gmail.com>
2024-11-28 15:28:21 +0100

    feat: metto asset github

--- release.config.js
+++ release.config.js
@@ -1,6 +1,9 @@
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
     "@semantic-release/github",
-     ["@semantic-release/git"]
+     "@semantic-release/git",
+     ["@semantic-release/github", {
+      "assets": ["dist/**/*.{js,css}", "docs", "package.json"]
+  }]
 )
 module.exports = config
