88c04f4bdf87cd8a06445459fd9d89abe80c6eb1
Elisa Albertini <elisa.albertini25@gmail.com>
2024-11-29 16:55:40 +0100

    feat: 22

--- release.config.js
+++ release.config.js
@@ -1,8 +1,9 @@
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
-  ["@semantic-release/git", {"assets": ["package.json"], "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"}],
-["@semantic-release/github", {"assets": ["WarehouseService/build/output/WarehouseService.jar"],
-  "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"}],
+  ["@semantic-release/git", {"assets": ["WarehouseService/build/output/WarehouseService.jar"], "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"}],
+  "@semantic-release/github"
+  //["@semantic-release/github", {"assets": ["WarehouseService/build/output/WarehouseService.jar"],
+  //"message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"}],
 //["@semantic-release/git", {
   //"assets": ["dist/**/*.{js,css}", "docs", "package.json"],
   //"message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
