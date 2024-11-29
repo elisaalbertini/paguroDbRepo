88a0df233463de301fc519227c15b269854bb0dc
Elisa Albertini <elisa.albertini25@gmail.com>
2024-11-29 13:00:50 +0100

    chore: pull request 9

--- release.config.js
+++ release.config.js
@@ -1,6 +1,5 @@
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
-    "@semantic-release/github",
      "@semantic-release/git",
      ["@semantic-release/github", {
       "assets": ["package.json"]
