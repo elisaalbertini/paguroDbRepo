120b70b0f8a2ca0ddb7c6cd6ad4ecad0131faf60
Elisa Albertini <elisa.albertini25@gmail.com>
2024-11-29 15:13:02 +0100

    feat: 4

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
 ["@semantic-release/git", {"assets": []}],
-     "@semantic-release/github"
+["@semantic-release/github", {"assets": []}],
 )
 module.exports = config
