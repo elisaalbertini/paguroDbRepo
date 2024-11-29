19db448f14d50b5da96f45f8db31edc932d08802
Elisa Albertini <elisa.albertini25@gmail.com>
2024-11-29 17:43:45 +0100

    feat: 27

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
-  ["@semantic-release/git", {"assets": ["CHANGELOpG.md"]}],
+  ["@semantic-release/git", {"assets": ["start.sh"]}],
   ["@semantic-release/github", {"assets": ["WarehouseService/build/output/WarehouseService.jar"],
   "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"}],
 //["@semantic-release/git", {
